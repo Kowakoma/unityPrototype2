b2825c17187d3ed9b96f10525eec369e459e90f6
Kowakoma <wilmoord@yandex.ru>
2025-06-19 19:14:43 +0300

    Added player conroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Controller input
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.left * horizontalInput * Time.deltaTime * speed);
+
+
+        //Set the limits of player's movement
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
     }
 }
