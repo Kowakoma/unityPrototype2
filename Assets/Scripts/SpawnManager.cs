ea58b553b1323f355c14bc6efbdaf59a3844efa3
Kowakoma <wilmoord@yandex.ru>
2025-06-30 17:51:23 +0300

    Dogs spawning use InvokeRepiting method instead S keydown

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,25 +6,30 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefaps;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Spawning random dog by keydown "S"
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            //Randomly genegate animalindex and spawn position
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
+            //Randomly genegate spawn position
             Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            //Randomly genegate animalIndex
             int animalIndex = Random.Range(0, animalPrefaps.Length);
+            //Spawn dog
             Instantiate(animalPrefaps[animalIndex], spawnPos, animalPrefaps[animalIndex].transform.rotation);
-        }
     }
 }
